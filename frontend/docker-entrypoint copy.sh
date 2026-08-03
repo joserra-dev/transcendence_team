f50e75c2d6da1bfa34c4caa723695b27c1812f16
Egoitz Larrea <elarrea-@student.42urduliz.com>
2026-08-03 11:41:36 +0200

    Eliminar fichero no valido

--- frontend/docker-entrypoint copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Reemplazar las variables de entorno en el template dentro de la carpeta assets
-envsubst '${URL_FRONT} ${URL_BACK}' < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
-
-# Ejecutar Nginx en primer plano
-exec nginx -g "daemon off;"
